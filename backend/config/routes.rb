91162528632011ea6b3ef2fd094604e811653ec9
sabakantaro <sbkryo290112@yahoo.co.jp>
2023-05-16 23:34:34 -0700

    Add comment function

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'api/v1/events#index' # Add root wit status 200 to deploy render
   namespace :api do
     namespace :v1 do
-      resources :events, only: %i[index create destroy]
+      resources :events, only: %i[index show create destroy] do
+        resources :comments, only: %i[create destroy]
+      end
       get 'events/index'
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations'
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_13_184043) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_17_043358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.text "description"
