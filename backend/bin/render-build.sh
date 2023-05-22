686659725afbe98124fb74b3f43b916219803b11
sabakantaro <sbkryo290112@yahoo.co.jp>
2023-05-22 09:38:08 -0700

    Fix the path of sh file

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -6,7 +6,7 @@ set -o errexit
 # bundle exec rake db:migrate
 
 export RUBY_VERSION=3.1.0 &&
-export BUNDLE_GEMFILE=$RENDER_SRC_ROOT/api/Gemfile &&
+export BUNDLE_GEMFILE=$RENDER_SRC_ROOT/Gemfile &&
 set_ruby_env $(fetch_or_build "3.1.0") &&
 /opt/render/project/rubies/ruby-3.1.0/bin/gem update --system &&
 /opt/render/project/rubies/ruby-3.1.0/bin/bundle config set --local deployment 'true'
