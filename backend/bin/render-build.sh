a59230edbd6354d9b75d0072ea8409c7b46b0e07
sabakantaro <sbkryo290112@yahoo.co.jp>
2023-05-22 09:29:33 -0700

    Update Ruby version

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -2,5 +2,13 @@
 # exit on error
 set -o errexit
 
-bundle install
+# bundle install
+# bundle exec rake db:migrate
+
+export RUBY_VERSION=3.1.0 &&
+export BUNDLE_GEMFILE=$RENDER_SRC_ROOT/api/Gemfile &&
+set_ruby_env $(fetch_or_build "3.1.0") &&
+/opt/render/project/rubies/ruby-3.1.0/bin/gem update --system &&
+/opt/render/project/rubies/ruby-3.1.0/bin/bundle config set --local deployment 'true'
+/opt/render/project/rubies/ruby-3.1.0/bin/bundle install &&
 bundle exec rake db:migrate
