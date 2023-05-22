4a40f50b9ec2c9548a0790bc47eebb6351bca66a
sabakantaro <sbkryo290112@yahoo.co.jp>
2023-05-22 01:06:30 -0700

    Fix render sh file and gem file

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -2,14 +2,5 @@
 # exit on error
 set -o errexit
 
-# Upgrade RubyGems to the latest version
-gem update --system
-
-# Install Bundler
-gem install bundler
-
-# Install project dependencies
-bundle update
-
-# Run any necessary build commands
+bundle install
 bundle exec rake db:migrate
