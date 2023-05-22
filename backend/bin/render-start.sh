11505fbe29525ee3a8ba1fbe03990a76ffb80f50
sabakantaro <sbkryo290112@yahoo.co.jp>
2023-05-22 10:18:15 -0700

    Fix the path of sh file

--- backend/bin/render-start.sh
+++ backend/bin/render-start.sh
@@ -4,7 +4,8 @@ set -o errexit
 
 export RUBY_VERSION=$(cat $RENDER_SRC_ROOT/backend/.ruby-version)
 export BUNDLE_GEMFILE=$RENDER_SRC_ROOT/backend/Gemfile
-set_ruby_env $(fetch_or_build "$RUBY_VERSION")
-gem install bundler
-gem update --system
-cd backend && bundle install && bundle exec rake db:migrate
+source /home/render/ruby-env.sh
+set_ruby_env "$RUBIES_ROOT/ruby-$RUBY_VERSION"
+
+cd $RENDER_SRC_ROOT/backend
+bundle exec puma -C ./config/puma.rb
